5aa69e46060efc1163208394cdc2311108b9a6da
Matteo Canessa <Matteo.Canessa@microarea.it>
2016-01-26 15:33:31 +0000

    Firma digitale MSI

---
+++ DigitalSignerClient/DigitalSignerClient.cs
@@ -0,0 +1,85 @@
+﻿using Microarea.TaskBuilderNet.ParametersManager;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microarea.DigitalSignerClient
+{
+    public class DigitalSignerClient
+    {
+        public static int Main(params string[] args)
+        {
+            if (args == null || args.Length != 1)
+            {
+                return 1;
+            }
+
+            try
+            {
+                SignFile(args[0]);
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 1;
+            }
+        }
+
+        public static void SignFile(string filePath)
+        {
+            using (var proxy = new DigitalSignerServer.DigitalSignerSoapClient())
+            {
+                (proxy.Endpoint.Binding as BasicHttpBinding).MaxReceivedMessageSize = 5000000;
+                var country = GetCountry();
+                var sessionManager = new ClickOnceDeployerSessionManager() { Country = country };
+                var sessionToken = Guid.NewGuid();
+
+                var dataFile = ReadFileAsByteArray(filePath);
+
+                var envelope = new ClickOnceDeployerEnvelope() { File = dataFile };
+
+                string result = proxy.SignBootstrapper(
+                        sessionManager.PackData(envelope, true, sessionToken),
+                        sessionToken.ToString("N", CultureInfo.InvariantCulture),
+                        country
+                        );
+
+                envelope = sessionManager.UnpackData(result, false, sessionToken);
+
+                if (!envelope.Error)
+                {
+                    SaveFile(envelope.File, filePath);
+                }
+            }
+        }
+
+        static void SaveFile(byte[] result, string filePath)
+        {
+            using (var bw = new BinaryWriter(File.OpenWrite(filePath)))
+            {
+                bw.Write(result, 0, result.Length);
+            }
+        }
+
+        static byte[] ReadFileAsByteArray(string filePath)
+        {
+            using (var br = new BinaryReader(File.OpenRead(filePath)))
+            {
+                byte[] buffer = new byte[br.BaseStream.Length];
+                br.Read(buffer, 0, buffer.Length);
+
+                return buffer;
+            }
+        }
+
+        static string GetCountry()
+        {
+            return Path.GetFileNameWithoutExtension(Path.GetTempFileName()).Substring(2, 2);
+        }
+    }
+}
---
+++ DigitalSignerClient/SessionManager.cs
@@ -0,0 +1,45 @@
+﻿using Microarea.TaskBuilderNet.ParametersManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microarea.DigitalSignerClient
+{
+    class SessionManager
+    {
+        string country;
+        IParametersManager parametersManager = new ParametersManagerV1();
+
+        public SessionManager(string country)
+        {
+            this.country = country;
+        }
+        //---------------------------------------------------------------------------
+        public string PackData(byte[] file, bool isOn, Guid sessionId)
+        {
+            string inputString = Convert.ToBase64String(file);
+
+            return parametersManager.SetParameter(
+                isOn,
+                inputString,
+                sessionId.ToString(),
+                this.country
+                );
+        }
+
+        //---------------------------------------------------------------------------
+        public byte[] UnpackData(string dataFile, bool isOn, Guid sessionId)
+        {
+            string outputString = parametersManager.GetParameter(
+                 isOn,
+                 dataFile,
+                 sessionId.ToString(),
+                 this.country
+                 );
+
+            return Convert.FromBase64String(outputString);
+        }
+    }
+}
