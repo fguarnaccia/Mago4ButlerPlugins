704d666f4bcebe09d50f235cbfb5c2c6d3cf2f7b
Matteo Canessa <Matteo.Canessa@microarea.it>
2016-07-19 14:25:12 +0000

    PAAS - Butler: modificata finestra di richiesta aggiornamenti

--- AutmaticUpdates/AskForUpdate.Designer.cs
+++ AutmaticUpdates/AskForUpdate.Designer.cs
@@ -29,40 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AskForUpdate));
-            this.lblUpdatesAvailable = new System.Windows.Forms.Label();
             this.btnInstall = new System.Windows.Forms.Button();
             this.btnNoThanks = new System.Windows.Forms.Button();
+            this.pnlButlerImg = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // lblUpdatesAvailable
-            // 
-            this.lblUpdatesAvailable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUpdatesAvailable.Location = new System.Drawing.Point(12, 48);
-            this.lblUpdatesAvailable.Name = "lblUpdatesAvailable";
-            this.lblUpdatesAvailable.Size = new System.Drawing.Size(366, 87);
-            this.lblUpdatesAvailable.TabIndex = 0;
-            this.lblUpdatesAvailable.Text = "A new version of Mago4Butler is available, do you want to proceed with the instal" +
-    "lation?";
             // 
             // btnInstall
             // 
+            this.btnInstall.BackColor = System.Drawing.Color.ForestGreen;
             this.btnInstall.DialogResult = System.Windows.Forms.DialogResult.Yes;
-            this.btnInstall.Location = new System.Drawing.Point(186, 209);
+            this.btnInstall.FlatAppearance.BorderColor = System.Drawing.Color.ForestGreen;
+            this.btnInstall.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstall.ForeColor = System.Drawing.Color.White;
+            this.btnInstall.Location = new System.Drawing.Point(155, 327);
             this.btnInstall.Name = "btnInstall";
-            this.btnInstall.Size = new System.Drawing.Size(75, 23);
+            this.btnInstall.Size = new System.Drawing.Size(111, 23);
             this.btnInstall.TabIndex = 1;
-            this.btnInstall.Text = "Install";
-            this.btnInstall.UseVisualStyleBackColor = true;
+            this.btnInstall.Text = "Yes, please";
+            this.btnInstall.UseVisualStyleBackColor = false;
             // 
             // btnNoThanks
             // 
             this.btnNoThanks.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnNoThanks.Location = new System.Drawing.Point(267, 209);
+            this.btnNoThanks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNoThanks.Location = new System.Drawing.Point(272, 327);
             this.btnNoThanks.Name = "btnNoThanks";
             this.btnNoThanks.Size = new System.Drawing.Size(111, 23);
             this.btnNoThanks.TabIndex = 1;
             this.btnNoThanks.Text = "Not now, thanks";
             this.btnNoThanks.UseVisualStyleBackColor = true;
+            // 
+            // pnlButlerImg
+            // 
+            this.pnlButlerImg.BackgroundImage = global::Microarea.Mago4Butler.AutomaticUpdates.Properties.Resources.UpdateRequest;
+            this.pnlButlerImg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pnlButlerImg.Location = new System.Drawing.Point(12, 12);
+            this.pnlButlerImg.Name = "pnlButlerImg";
+            this.pnlButlerImg.Size = new System.Drawing.Size(366, 300);
+            this.pnlButlerImg.TabIndex = 2;
             // 
             // AskForUpdate
             // 
@@ -71,16 +76,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.CancelButton = this.btnNoThanks;
-            this.ClientSize = new System.Drawing.Size(390, 244);
+            this.ClientSize = new System.Drawing.Size(390, 362);
+            this.Controls.Add(this.pnlButlerImg);
             this.Controls.Add(this.btnNoThanks);
             this.Controls.Add(this.btnInstall);
-            this.Controls.Add(this.lblUpdatesAvailable);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(406, 282);
+            this.MaximumSize = new System.Drawing.Size(406, 400);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(406, 282);
+            this.MinimumSize = new System.Drawing.Size(406, 400);
             this.Name = "AskForUpdate";
             this.Text = "Updates available";
             this.ResumeLayout(false);
@@ -88,9 +93,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblUpdatesAvailable;
         private System.Windows.Forms.Button btnInstall;
         private System.Windows.Forms.Button btnNoThanks;
+        private System.Windows.Forms.Panel pnlButlerImg;
     }
 }
