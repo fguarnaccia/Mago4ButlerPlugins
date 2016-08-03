d01539e551ad42d04d4d2406bc315b79dc49497d
Matteo Canessa <Matteo.Canessa@microarea.it>
2016-08-03 14:35:18 +0000

    PAAS: implementata telemetria per uso interno di Mago4Butler

---
+++ Telemetry/Telemetry.cs
@@ -0,0 +1,45 @@
+﻿using Microarea.Mago4Butler.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microarea.Mago4Butler.Telemetry
+{
+    public class Telemetry : Mago4ButlerPlugin
+    {
+        public override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            var thread = new Thread(new ThreadStart(() =>
+            {
+                try
+                {
+                    StoreTelemetry();
+                }
+                catch
+                {}
+            }));
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        void StoreTelemetry()
+        {
+            string version = App.Instance.GetVersion("Mago4Butler").ToString();
+
+            using (var svc = new PAASUpdates.TelemetryService())
+            {
+#if DEBUG
+                svc.Url = "http://usr-canessamat1/PAASUpdates/TelemetryService.asmx";
+#else
+                svc.Url = "http://spp-hotfix/PAASUpdates/TelemetryService.asmx";
+#endif
+                svc.StoreTelemetryData(new PAASUpdates.TelemetryData() { MachineName = Environment.MachineName, Mago4ButlerVersion = version });
+            }
+        }
+    }
+}
