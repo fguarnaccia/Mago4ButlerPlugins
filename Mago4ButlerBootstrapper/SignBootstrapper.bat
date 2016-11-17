2c2cdd504c4a01c9b4f3c55b217ef5bd043c8986
Matteo Canessa <Matteo.Canessa@microarea.it>
2016-11-17 11:11:55 +0000

    Fix generazione setup  per firma digitale

--- Mago4ButlerBootstrapper/SignBootstrapper.bat
+++ Mago4ButlerBootstrapper/SignBootstrapper.bat
@@ -2,7 +2,9 @@
 @REM %2 Certificate file full path
 
 "%WIX%bin\insignia.exe" -ib %1 -o %TEMP%\engine.exe
-call C:\DigitalSign.bat %2 %TEMP%\engine.exe
+@REM call DigitalSign.bat %2 %TEMP%\engine.exe
+call ..\DigitalSignerClient\bin\Release\Microarea.DigitalSignerClient.exe %TEMP%\engine.exe
 "%WIX%bin\insignia.exe"  -ab %TEMP%\engine.exe %1 -o %1
-call C:\DigitalSign.bat %2 %1
-del %TEMP%\engine.exe
+@REM call DigitalSign.bat %2 %1
+call ..\DigitalSignerClient\bin\Release\Microarea.DigitalSignerClient.exe %1
+@REM del %TEMP%\engine.exe
