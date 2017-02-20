7ca0c2bf4cad683ece1c05e8bcf6eebb6ffa64e4
Matteo Canessa <Matteo.Canessa@microarea.it>
2017-02-20 13:23:36 +0000

    Nuova versione

--- Telemetry/Properties/AssemblyInfo.cs
+++ Telemetry/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// [assembly: AssemblyVersion("1.0.6119.18651")]
-[assembly: AssemblyVersion("1.0.6119.18651")]
+// [assembly: AssemblyVersion("1.0.6120.18651")]
+[assembly: AssemblyVersion("1.0.6120.18651")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
