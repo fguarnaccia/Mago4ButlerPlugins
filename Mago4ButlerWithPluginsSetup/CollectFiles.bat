e47de09c9cab86807dafbb2aad9add692684c495
Matteo Canessa <Matteo.Canessa@microarea.it>
2017-03-22 15:34:54 +0000

    Firma digitale dll prima di realizzare il master

--- Mago4ButlerWithPluginsSetup/CollectFiles.bat
+++ Mago4ButlerWithPluginsSetup/CollectFiles.bat
@@ -10,4 +10,8 @@ ECHO Platform = %3
 ECHO source directory = "%2Mago4Butler\bin\%3\%1"
 ECHO output file = "%2%4\Files.wxs"
 ECHO transform file = "%2%4\Transform.xslt"
+
+FOR %%i IN (%2Mago4Butler\bin\%3\%1\*.dll) DO %2DigitalSignerClient\bin\Release\Microarea.DigitalSignerClient.exe %%i
+FOR %%i IN (%2Mago4Butler\bin\%3\%1\*.exe) DO %2DigitalSignerClient\bin\Release\Microarea.DigitalSignerClient.exe %%i
+
 "%WIX%bin\heat.exe" dir "%2Mago4Butler\bin\%3\%1" -configuration %1 -platform %3 -cg ProductComponents -dr INSTALLLOCATION -var var.M4BSourceDir -gg -g1 -sfrag -scom -sreg -srd -template fragment -t "%2%4\Transform.xslt" -out "%2%4\Files.wxs"
