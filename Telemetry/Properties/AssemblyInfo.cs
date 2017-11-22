82cccbccb8d1e323e756b112c198099646b73786
Matteo Canessa <Matteo.Canessa@microarea.it>
2017-11-22 16:27:34 +0000

    Master

--- Telemetry/Properties/AssemblyInfo.cs
+++ Telemetry/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// [assembly: AssemblyVersion("1.0.6156.18651")]
-[assembly: AssemblyVersion("1.0.6156.18651")]
+// [assembly: AssemblyVersion("1.0.6158.18651")]
+[assembly: AssemblyVersion("1.0.6158.18651")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
