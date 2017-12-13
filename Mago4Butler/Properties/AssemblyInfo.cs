92d4eaad72c815a4ca1d59a3abd4bb9bbac4b93a
Matteo Canessa <Matteo.Canessa@microarea.it>
2017-12-13 16:39:08 +0000

    Nuovo master

--- Mago4Butler/Properties/AssemblyInfo.cs
+++ Mago4Butler/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.6098.18522")]
-[assembly: AssemblyVersion("1.0.6098.18522")]
+// [assembly: AssemblyVersion("1.0.6102.18522")]
+[assembly: AssemblyVersion("1.0.6102.18522")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
