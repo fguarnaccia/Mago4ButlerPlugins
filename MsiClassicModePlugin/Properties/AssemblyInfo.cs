b540fbe7d2c93e2cc4695ad5e2a11b9af1ed941d
Fabio Guarnaccia <Fabio.Guarnaccia@microarea.it>
2018-01-12 13:48:31 +0000

    Deploy

--- MsiClassicModePlugin/Properties/AssemblyInfo.cs
+++ MsiClassicModePlugin/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.6072.1117")]
-[assembly: AssemblyFileVersion("1.0.6072.1117")]
+[assembly: AssemblyVersion("1.0.2018.0112")]
+[assembly: AssemblyFileVersion("1.0.2018.0112")]
