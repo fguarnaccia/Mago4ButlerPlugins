86130b6790c3bc506fa1eebbbaf2226054852ad3
Fabio Guarnaccia <Fabio.Guarnaccia@microarea.it>
2018-01-12 08:52:59 +0000

    Aggiunta gestione titolo, attivato url per M4Web

--- MagoRunnerPlugin/MagoRunnerplugin.cs
+++ MagoRunnerPlugin/MagoRunnerplugin.cs
@@ -5,8 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
-
-
+using System.Xml.Linq;
+using System.Linq;
 namespace MagoRunnerPlugin
 {
     public class MagoRunner : Mago4ButlerPlugin
@@ -31,17 +31,20 @@ namespace MagoRunnerPlugin
                 );
             cmis.Add(cmi);
 
-            cmi.Name = "runSetTitle";
-            cmi.Text = "set as Title";
-            cmi.ShortcutKeys = Keys.Control | Keys.T;
-            cmi.Command = new Action<Instance>(
-                (instance)
-                =>
-                runSetTitle(instance)
-                );
-            cmis.Add(cmi);
+            #region Set Title
+            //cmi = new ContextMenuItem();
+            //cmi.Name = "runSetTitle";
+            //cmi.Text = "set as Title (CapsLock to restore)";
+            //cmi.ShortcutKeys = Keys.Control | Keys.T;
+            //cmi.Command = new Action<Instance>(
+            //    (instance)
+            //    =>
+            //    RunSetTitle(instance)
+            //    );
+            //cmis.Add(cmi);
+            #endregion
 
-
+            #region Madico
             cmi = new ContextMenuItem();
             cmi.Name = "runMadico";
             cmi.Text = "...Check Instance";
@@ -52,7 +55,9 @@ namespace MagoRunnerPlugin
                 RunMadico(instance)
                 );
             cmis.Add(cmi);
+            #endregion
 
+            #region COD
             cmi = new ContextMenuItem();
             cmi.Name = "runCOD";
             cmi.Text = "...ClickOnceDeployer";
@@ -62,9 +67,10 @@ namespace MagoRunnerPlugin
                 =>
                 RunCOD(instance)
                 );
-
             cmis.Add(cmi);
+            #endregion
 
+            #region Mago
             cmi = new ContextMenuItem();
             cmi.Name = "runMago";
             cmi.Text = "...Mago";
@@ -75,7 +81,9 @@ namespace MagoRunnerPlugin
                 RunMago(instance)
                 );
             cmis.Add(cmi);
+            #endregion
 
+            #region Console
             cmi = new ContextMenuItem();
             cmi.Name = "runConsole";
             cmi.Text = "...Admin Console";
@@ -86,7 +94,9 @@ namespace MagoRunnerPlugin
                 RunConsole(instance)
                 );
             cmis.Add(cmi);
+            #endregion
 
+            #region Custom
             cmi = new ContextMenuItem();
             cmi.Name = "runCustom";
             cmi.Text = "...Custom";
@@ -97,9 +107,10 @@ namespace MagoRunnerPlugin
                 RunCustom(instance)
                 );
             cmis.Add(cmi);
+            #endregion
 
+            #region Publish
             cmi = new ContextMenuItem();
-
             cmi.Name = "runPublish";
             cmi.Text = "...Publish";
             cmi.ShortcutKeys = Keys.Control | Keys.P;
@@ -109,7 +120,9 @@ namespace MagoRunnerPlugin
                 RunPublish(instance)
                 );
             cmis.Add(cmi);
+            #endregion
 
+            #region Standard
             cmi = new ContextMenuItem();
             cmi.Name = "runStandard";
             cmi.Text = "...Standard";
@@ -120,7 +133,9 @@ namespace MagoRunnerPlugin
                 RunStandard(instance)
                 );
             cmis.Add(cmi);
+            #endregion
 
+            #region Web
             cmi = new ContextMenuItem();
             cmi.Name = "runMagoWeb";
             cmi.Text = "...Easylook/MagoWeb";
@@ -130,37 +145,77 @@ namespace MagoRunnerPlugin
                 =>
                 RunMagoWeb(instance)
                 );
-            cmis.Add(cmi);
+            cmis.Add(cmi); 
+            #endregion
 
-             return cmis;
+            return cmis;
         
         }
 
-        private void runSetTitle(Instance instance)
+     
+        private void RunSetTitle(Instance instance)
         {
             string BrandMn = @"Standard\Applications\ERP\Solutions\main.brand.xml";
-            string BrandM4 = @"Standard\Applications\ERP\Solutions\Mago4-Pro.Brand.xml";
-
-           
+            string BrandM4Pro = @"Standard\Applications\ERP\Solutions\Mago4-Pro.Brand.xml";
+            string BrandM4Ent = @"Standard\Applications\ERP\Solutions\Mago4-Ent.Brand.xml";
+                     
             string xmlfile = string.Empty;
-            XmlDocument domdoc = new XmlDocument();
-            if (IsMago4(instance.Version))
+            bool RestoreDefault = Control.IsKeyLocked(Keys.CapsLock);
+        
+            if (instance.ProductType == Microarea.Mago4Butler.Model.ProductType.Mago4)
             {
                 
-                xmlfile = Path.Combine(App.Instance.Settings.RootFolder, instance.Name, BrandMn); ;
+                xmlfile = Path.Combine(App.Instance.Settings.RootFolder, instance.Name, BrandM4Pro);
+                SetM4Title(xmlfile, instance.Name, RestoreDefault);
+                xmlfile = Path.Combine(App.Instance.Settings.RootFolder, instance.Name, BrandM4Ent);
+                SetM4Title(xmlfile, instance.Name, RestoreDefault);
 
             }
             else
             {
-                xmlfile = Path.Combine(App.Instance.Settings.RootFolder, instance.Name, BrandM4); 
+                xmlfile = Path.Combine(App.Instance.Settings.RootFolder, instance.Name, BrandMn);              
+                SetMnTitle(xmlfile, instance.Name, RestoreDefault);
+
             }
-    
 
-                domdoc.Load(xmlfile);
-
-       
         }
 
+        internal void SetM4Title(string BrandFile, string Title, bool RestoreDefaultTitle)
+
+        {
+            const string cstBrandM4 = "Mago4";
+            if (RestoreDefaultTitle) Title = cstBrandM4;
+       
+            XElement xe = XElement.Load(BrandFile);
+
+            var brands =
+                from el in xe.Descendants("BrandedKey")
+
+                where el.Attribute("source").Value == "M4"
+               select el;
+
+            var element = brands.FirstOrDefault();
+            element.Attribute("branded").Value = Title;
+
+            xe.Save(BrandFile);
+        }
+
+        internal void SetMnTitle(string BrandFile, string Title, bool RestoreDefaultTitle)
+        {
+            const string cstBrandMn = "Mago.net";
+
+            XmlDocument domdoc = new XmlDocument();
+            domdoc.Load(BrandFile);
+
+            if (RestoreDefaultTitle) Title = cstBrandMn;
+           
+            domdoc.Load(BrandFile);
+            if (domdoc.GetElementsByTagName("BrandedKeys")[0].FirstChild.Attributes.GetNamedItem("source").Value == "MN")
+                domdoc.GetElementsByTagName("BrandedKeys")[0].FirstChild.Attributes.GetNamedItem("branded").Value = Title;
+
+            domdoc.Save(BrandFile);
+
+        }
         private void RunNothing(Instance instance)
         {
             return;
@@ -175,7 +230,6 @@ namespace MagoRunnerPlugin
             return dch;
                  
         }
-
 
 
         void RunRoot(Instance istanza)
@@ -194,17 +248,18 @@ namespace MagoRunnerPlugin
             string fileName = string.Empty;
             string executable = string.Empty;
 
+            RunSetTitle(istanza);
+
+
             if (IsMago4(istanza.Version))
 
             {
                 executable = "TbAppManager.exe";
             
-
             }
             else
             {
                 executable = "Mago.Net.Exe";
-  
             }
 
             fileName = Path.Combine(App.Instance.Settings.RootFolder, istanza.Name, @"Apps\Publish\", executable);
@@ -223,15 +278,13 @@ namespace MagoRunnerPlugin
             {
                 executable = "AdministrationConsole.exe";
      
-
             }
             else
             {
                 executable = "MicroareaConsole.exe";
 
             }
-
-            
+         
             fileName = Path.Combine(App.Instance.Settings.RootFolder, istanza.Name, @"Apps\Publish\", executable);
 
             Process.Start(fileName,arguments);
@@ -275,8 +328,7 @@ namespace MagoRunnerPlugin
 
             if (IsMago4 (istanza.Version))
             {
-                return;
-                //FileName = "http://localhost:" + porta + "/" + istanza.Name + "/MagoWeb.aspx";
+                FileName = "http://localhost:" + porta + "/" + istanza.Name + "/M4Client/index.html";
 
             }
             else
@@ -314,7 +366,6 @@ namespace MagoRunnerPlugin
                 return true;
             }
 
-            
             return false;
 
         }
